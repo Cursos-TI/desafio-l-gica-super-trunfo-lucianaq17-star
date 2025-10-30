ef20a97cc7bcba140badb78da8da8f1ed286af13
Luciana Quirino <lucianaq17@gmail.com>
2025-10-30 22:49:31 +0000

    Nivel Intermediario

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,13 +6,13 @@ int main() {
   
 //Variáveis
 
-char pais1 [50], codigo1 [50],cidade1 [50], pais2 [50], codigo2[50], cidade2 [50];
+char pais1 [50], pais2 [50];
 int pontos1, pontos2;
 unsigned long int populacao1, populacao2;
 float pib1, area1, densidade1, percapita1, poder1, pib2, area2, densidade2, percapita2, poder2;
 unsigned long int populacaoresult;
 float arearesult, pibresult, densidaderesult, percapitaresult, poderresult;
-int pontosresult, atributo;
+int pontosresult, atributo1, atributo2;
 
 
 printf("\n*** Jogo Super Trunfo ***\n");
@@ -23,11 +23,6 @@ printf("\nDigite Dados da Carta 1 \n");
 printf("País: ");
 scanf("%s", pais1);
 
-printf("Cidade: ");
-scanf("%s",cidade1);
-
-printf("Código: ");
-scanf("%s",codigo1);
 
 printf("População: ");
 scanf("%lu", &populacao1);
@@ -47,12 +42,6 @@ printf("\nDigite Dados da Carta 2 \n");
 
 printf("País: ");
 scanf("%s", pais2);
-
-printf("Cidade: ");
-scanf("%s",cidade2);
-
-printf("Código: ");
-scanf("%s",codigo2);
 
 printf("População: ");
 scanf("%lu", &populacao2);
@@ -78,20 +67,20 @@ percapita2 = pib2/populacao2;
 poder2 = populacao2 + area2 + pib2 + pontos2 + percapita2 +1/densidade2;
 
 //Escolha do Atributo
-printf("\nPor favor, escolha um atributo.\n");
+printf("\nPor favor, escolha o primeiro atributo.\n");
 printf("1. População\n");
 printf("2. Área\n");
 printf("3. Pib\n");
 printf("4. Pontos Turísticos\n");
 printf("5. Densidade\n");
 printf("6. Pib Per Capita\n");
-scanf("%d", &atributo);
+scanf("%d", &atributo1);
 
 
 //Comparação de cartas
 printf("\n*** Batalha de Cartas ***\n");
 
-switch (atributo){
+switch (atributo1){
 case 1:
   printf("População \n");
   if(populacao1>populacao2){
@@ -144,18 +133,12 @@ if (percapita1>percapita2) {
 }else{
     printf("Pib Per Capita: Carta 2 venceu!\n");
 }
-default:
-  printf("Opção Inválida\n");
-break;
-}
 
 
 
 // Saída carta 1
 printf("\nResultado da Carta 1 \n");
 printf("País: %s\n", pais1);
-printf("Cidade: %s\n", cidade1);
-printf("Código: %s\n", codigo1);
 printf("População: %d\n", populacao1);
 printf("Área: %.2f Km2\n",area1);
 printf("Pib: %.2f reais\n",pib1);
@@ -168,8 +151,6 @@ printf("Poder da Carta 1: %.2f\n", poder1);
 //Saída carta 2
 printf("\nResultado da Carta 2 \n");
 printf("País: %s\n", pais2);
-printf("Cidade: %s\n",cidade2);
-printf("Código: %s\n", codigo2);
 printf("População: %d\n", populacao2);
 printf("Área: %.2f Km2\n",area2);
 printf("Pib: %.2f reais\n",pib2);
@@ -177,6 +158,11 @@ printf("Pontos Turísticos: %d\n", pontos2);
 printf("Densidade: %.2f hab/Km2\n", densidade2);
 printf("Pib Per Capita: %.2f reais\n", percapita2);
 printf("Poder da Carta 2: %.2f\n", poder2);
+
+default:
+  printf("Opção Inválida\n");
+break;
+}
 
 
 
